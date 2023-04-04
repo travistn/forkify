f9cc79d40445a35372732c513f8acb6358555686
travistn <travisn714@gmail.com>
2023-04-04 16:49:56 -0700

    useWindowWide hook finds the width of current screen

---
+++ hooks/useWindowWide.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export const useWindowWide = (size: number) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setWidth]);
+
+  return width;
+};
