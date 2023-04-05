05d599d3b4f7bcf210df251930c67a57e3a998ac
travistn <travisn714@gmail.com>
2023-04-05 14:11:58 -0700

    Changed main tag to div tag so that there is only one main tag on website

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import RecipeBook from '@/components/RecipeBook';
 
 export default function Home() {
   return (
-    <main className='bg-gradient-to-br from-[#fbdb89] to-[#f48982] xl:py-16 2xl:py-[5rem]'>
+    <div className='bg-gradient-to-br from-[#fbdb89] to-[#f48982] xl:py-16 2xl:py-[5rem]'>
       <RecipeBook />
-    </main>
+    </div>
   );
 }
