35c2060eba727ca2c3990dc1b91706239682d6ac
travistn <travisn714@gmail.com>
2023-04-05 14:12:21 -0700

    Created a recipes component for the recipe book

--- components/RecipeBook.tsx
+++ components/RecipeBook.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import RecipeBookHeader from './RecipeBookHeader';
+import Recipes from './Recipes';
 
 export default function RecipeBook() {
   return (
-    <section className='w-full min-h-screen bg-pale-orange xl:rounded-md xl:w-[80%] xl:mx-auto 2xl:w-[64%]'>
+    <main className='w-full min-h-screen bg-pale-orange xl:rounded-md xl:w-[80%] xl:mx-auto 2xl:w-[64%]'>
       <RecipeBookHeader />
-    </section>
+      <section className='w-full flex flex-row'>
+        <Recipes />
+      </section>
+    </main>
   );
 }
---
+++ components/Recipes.tsx
@@ -0,0 +1,3 @@
+export default function Recipes() {
+  return <section className='bg-white h-screen w-[34%]'></section>;
+}
