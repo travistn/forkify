2e8bb8ec7de7374edc557409520b3440a4eafff4
travistn <travisn714@gmail.com>
2023-04-06 16:49:24 -0700

    Implemented an animation for the recipe card when hovering

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -2,17 +2,17 @@ import { RecipeCardProps } from '@/common/types';
 
 export default function RecipeCard({ title, publisher, image_url }: RecipeCardProps) {
   return (
-    <li className='flex flex-row items-center p-4 md:bg-pale-orange'>
+    <li className='flex flex-row items-center p-4 cursor-pointer transition-all duration-200 ease-in hover:-translate-y-1 hover:bg-pale-orange'>
       <img
         src={image_url}
         alt='recipe'
         className='w-[50px] h-[50px] rounded-full object-cover md:w-[60px] md:h-[60px]'
       />
       <div className='flex flex-col gap-2 ml-4 min-w-0'>
-        <h2 className='uppercase text-[13px] font-semibold text-pink-orange truncate md:text-[15px]'>
+        <h2 className='uppercase text-[13px] font-semibold text-pink-orange truncate lg:text-[15px]'>
           {title}
         </h2>
-        <h3 className='uppercase text-[10px] font-semibold text-black/60 md:text-[12px]'>
+        <h3 className='uppercase text-[10px] font-semibold text-black/60 lg:text-[12px]'>
           {publisher}
         </h3>
       </div>
