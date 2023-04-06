1e94dd72f4eda0e40c0dd46d152334267ef8a2f9
travistn <travisn714@gmail.com>
2023-04-05 17:17:05 -0700

    Changed RecipeCardProps from a type to an interface is now an import

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,25 +1,19 @@
-type RecipeCardProps = {
-  recipe: {
-    title: string;
-    publisher: string;
-    image_url: string;
-  };
-};
+import { RecipeCardProps } from '@/common/types';
 
-export default function RecipeCard({ recipe }: RecipeCardProps) {
+export default function RecipeCard({ title, publisher, image_url }: RecipeCardProps) {
   return (
     <li className='flex flex-row items-center p-4 md:bg-pale-orange'>
       <img
-        src={recipe?.image_url}
+        src={image_url}
         alt='recipe'
         className='w-[50px] h-[50px] rounded-full object-cover md:w-[60px] md:h-[60px]'
       />
       <div className='flex flex-col gap-2 ml-4 min-w-0'>
         <h2 className='uppercase text-[13px] font-semibold text-pink-orange truncate md:text-[15px]'>
-          {recipe?.title}
+          {title}
         </h2>
         <h3 className='uppercase text-[10px] font-semibold text-black/60 md:text-[12px]'>
-          {recipe?.publisher}
+          {publisher}
         </h3>
       </div>
     </li>
