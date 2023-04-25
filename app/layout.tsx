be2b1dbc1167e10d514c087fa4bbbfca96b854f6
travistn <travisn714@gmail.com>
2023-04-25 14:44:25 -0700

    Changed the layout so that the recipe book will be on the home page by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito_Sans } from 'next/font/google';
 
 import './globals.css';
+import RecipeBookHeader from '@/components/RecipeBookHeader';
+import Recipes from '@/components/Recipes';
 
 const nunitoSans = Nunito_Sans({ subsets: ['latin'], weight: ['400', '600', '700'] });
 
@@ -9,10 +11,22 @@ export const metadata = {
   description: 'A recipe application developed by Travis Nguyen.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={nunitoSans.className}>
-      <body>{children}</body>
+      <body>
+        <div className='bg-gradient-to-br from-[#fbdb89] to-[#f48982] xl:py-16 2xl:py-[5rem]'>
+          <main className='w-full min-h-screen bg-pale-orange xl:rounded-md xl:w-[80%] xl:mx-auto 2xl:w-[64%]'>
+            <RecipeBookHeader />
+            <section className='w-full flex flex-col md:flex-row'>
+              <Recipes />
+              {children}
+            </section>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
+
+export default RootLayout;
