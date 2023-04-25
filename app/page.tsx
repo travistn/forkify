1002af541979c9f6f129108d4d27b8643362f7c2
travistn <travisn714@gmail.com>
2023-04-25 14:44:59 -0700

    When on the home page route the page will instruct users to search a recipe to begin

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import RecipeBook from '@/components/RecipeBook';
+'use client';
 
-export default function Home() {
+import { FaceSmileIcon } from '@heroicons/react/24/outline';
+
+function Home() {
   return (
-    <div className='bg-gradient-to-br from-[#fbdb89] to-[#f48982] xl:py-16 2xl:py-[5rem]'>
-      <RecipeBook />
-    </div>
+    <section className='w-full'>
+      <h2 className='flex flex-row items-center gap-2 px-8 pt-12 text-[14px] text-[#615551] font-semibold md:px-[8rem] lg:text-[18px] lg:gap-4'>
+        <FaceSmileIcon className='w-[40px] stroke-pink-orange stroke-2' />
+        Start by searching for a recipe or an ingredient. Have fun!
+      </h2>
+    </section>
   );
 }
+
+export default Home;
