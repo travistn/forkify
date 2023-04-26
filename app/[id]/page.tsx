db8ff6d6db6760169f873b7d0c98d7f704c83648
travistn <travisn714@gmail.com>
2023-04-26 16:30:43 -0700

    Updated API to v2

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -8,16 +8,24 @@ import { fetcher } from '@/utils/fetcher';
 function RecipeIdPage() {
   const params = useParams();
 
-  const { data } = useSWR(`https://forkify-api.herokuapp.com/api/get?rId=${params.id}`, fetcher);
+  const { data } = useSWR(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${params.id}?key=${process.env.NEXT_PUBLIC_FORKIFY_ENDPOINT}`,
+    fetcher
+  );
 
   return (
     <section className='w-full'>
       <figure className='bg-gradient-to-br from-[#fbdb89] to-[#f48982]'>
         <img
-          src={data?.recipe.image_url}
+          src={data?.data.recipe.image_url}
           className='w-full h-[200px] object-cover opacity-40 md:h-[260px] lg:h-[320px]'
         />
       </figure>
+      <section className='p-8'>
+        <div className='flex flex-row gap-8'>
+          <div>{data?.data.recipe.cooking_time}</div>
+        </div>
+      </section>
     </section>
   );
 }
