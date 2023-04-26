1b50258198f08dd1afd4560e8ca31908a7f598cd
travistn <travisn714@gmail.com>
2023-04-25 21:24:34 -0700

    Added matching recipe image to the recipe page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -10,7 +10,16 @@ function RecipeIdPage() {
 
   const { data } = useSWR(`https://forkify-api.herokuapp.com/api/get?rId=${params.id}`, fetcher);
 
-  return <div></div>;
+  return (
+    <section className='w-full'>
+      <figure className='bg-gradient-to-br from-[#fbdb89] to-[#f48982]'>
+        <img
+          src={data?.recipe.image_url}
+          className='w-full h-[200px] object-cover opacity-40 md:h-[260px] lg:h-[320px]'
+        />
+      </figure>
+    </section>
+  );
 }
 
 export default RecipeIdPage;
