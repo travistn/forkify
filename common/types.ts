aa7f6f2d5504051d4bef7379bb2f9008a9548634
travistn <travisn714@gmail.com>
2023-04-27 17:56:08 -0700

    Created RecipeIngredientsProps

--- common/types.ts
+++ common/types.ts
@@ -4,3 +4,9 @@ export interface RecipeCardProps {
   image_url: string;
   id: string;
 }
+
+export interface RecipeIngredientsProps {
+  quantity?: number;
+  unit: string;
+  description: string;
+}
