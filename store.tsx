49846cf6562561da52f56950da16764aab8842a3
travistn <travisn714@gmail.com>
2023-05-02 15:14:16 -0700

    Created helper function addBookmark that adds a bookmark into the bookmarks array

--- store.tsx
+++ store.tsx
@@ -1,13 +1,22 @@
 import { create } from 'zustand';
 
+interface Bookmark {
+  recipe: string;
+}
+
 interface AppState {
   search: string;
+  bookmarks: Bookmark[];
   enterSearch: (search: string) => void;
+  addBookmark: (bookmark: string) => void;
 }
 
 const useStore = create<AppState>()((set) => ({
   search: '',
-  enterSearch: (search: string) => set((state) => ({ search: (state.search = search) })),
+  bookmarks: [],
+  enterSearch: (search) => set((state) => ({ search: (state.search = search) })),
+  addBookmark: (bookmark) =>
+    set((state) => ({ bookmarks: [...state.bookmarks, { recipe: bookmark }] })),
 }));
 
 export default useStore;
